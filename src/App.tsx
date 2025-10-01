0f5993953fe2180dfd8dbda8b7ea554db3ce3d20
Juan Sebastian Tovar Monje <1006072266@u.icesi.edu.co>
2025-10-01 09:02:28 -0500

    feat: vite añadido

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import './App.css'
+
 function App() {
     return(
         <p>
-            temp
+            <h1 className='bg-blue-500  sm:bg-red-500 hover:bg-yellow-100'>Hola Tailwind!</h1>
         </p>
     )
 }
