29a0a585804d36ce3e490c9a50de684aedf6d55e
Juan Tovar <juanstovarm@gmail.com>
2025-10-03 16:56:07 -0500

    feat: app aditions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Feed from './Pages/Feed'
 
 function App() {
     return(
-        <p>
-            <h1 className='bg-blue-500  sm:bg-red-500 hover:bg-yellow-100'>Hola Tailwind!</h1>
-        </p>
+        <>
+        
+        </>
     )
 }
 
