4923139a7a96802ee60a975b97fab5a75f45f55d
Juan Tovar <juanstovarm@gmail.com>
2025-10-03 15:00:56 -0500

    feat: Added pages forlder and feed.tsx

---
+++ src/Pages/Feed.tsx
@@ -0,0 +1,12 @@
+
+import PostComponent from "../components/Feed-Components/Post-Component";
+
+function Feed(){
+    return(
+    <div>
+    <PostComponent text="De la cáscara de un banano… ¡A las páginas de una de las revistas científicas más reconocidas del mundo!" img="https://scontent.fclo9-1.fna.fbcdn.net/v/t51.82787-15/543650830_18528441820028716_3973655390271086394_n.webp?stp=dst-jpg_tt6&_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeE52ggEspcCxVvvh_NDYt0Q05-XRYfME7XTn5dFh8wTtUJWnDPrsl7J55duREMEg-uX3iAUujdU65yqBTVNcE-V&_nc_ohc=d3gVcJ32wUAQ7kNvwG0rx0W&_nc_oc=AdlgOkMpq7-X1045ndOQ2II7g8d3Nn3aDmK8_GW0utNbM80dYCT-b43GygaKjesheMo&_nc_zt=23&_nc_ht=scontent.fclo9-1.fna&_nc_gid=UdxSBTKhLRoOLWyKHXQOXg&oh=00_Afe-MDTfcF4FmIdl3H2iTyOsxrKk_TDXknOuGBOWYYU2BQ&oe=68E514F9"/>
+    </div>
+    )
+}
+
+export default Feed
