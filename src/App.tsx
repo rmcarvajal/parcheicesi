3360de37a702fe6173583b39f0608f095f538152
Daniel <135277854+Zaphkiel2200@users.noreply.github.com>
2025-10-03 21:50:00 -0500

    feat: header + assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/navegation/Header';
+import Navbar from './components/Navbar'; 
+import './App.css';
 
 function App() {
-    return(
-        <p>
-            <h1 className='bg-blue-500  sm:bg-red-500 hover:bg-yellow-100'>Hola Tailwind!</h1>
-        </p>
-    )
+  return (
+    <div className="app">
+      <Header title="" />
+      <main className="main-content">
+        <Routes>
+          {/* rutas de tabs del header */}
+          <Route path="/events" element={<div>Página de Eventos</div>} />
+          <Route path="/campus" element={<div>Página de Campus</div>} />
+          <Route path="/noticias" element={<div>Página de Noticias</div>} />
+          <Route path="/donar" element={<div>Página de Donar</div>} />
+        </Routes>
+      </main>
+      <Navbar /> 
+    </div>
+  );
 }
 
-export default App
+export default App;
