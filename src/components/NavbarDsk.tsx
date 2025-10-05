7c1217be76bab95a6eb0ab71880d060ccae13643
Juan Tovar <juanstovarm@gmail.com>
2025-10-05 13:17:08 -0500

    feat: nueva navbar para desktop incluida

---
+++ src/components/NavbarDsk.tsx
@@ -0,0 +1,44 @@
+import { NavLink } from 'react-router-dom';
+
+
+function NavBarDsk() {
+  return (
+    <div id='navbar-container' className='flex flex-col min-w-20 h-screen p-7.5 bg-white'>
+        <img src='src\Assets\LOGO horizontal.png' className='h-22 w-auto object-contain mb-8'></img>
+        <nav id="nav-bar" className='flex flex-col w-full justify-around '>
+      
+      {/* Usamos NavLink y tipamos el argumento de la función 'style' o 'className'. 
+        React Router proporciona la interfaz 'NavLinkRenderProps' implícitamente.
+      */}
+      <NavLink 
+        to="/" 
+        className="text-brand font-bold text-2xl w-fit my-1 hover:pl-1.5 hover:pr-5.5 hover:bg-orange hover:text-white ease-in transition-transform hover:translate-x-2"
+        >
+        Inicio
+      </NavLink>
+
+        <NavLink 
+        to="/messages" 
+        className="text-brand font-bold text-2xl w-fit my-1 hover:pl-1.5 hover:pr-5.5 hover:bg-yellow hover:text-black ease-in transition-transform hover:translate-x-2"
+        >
+        Mensajes
+      </NavLink>
+
+      <NavLink 
+        to="/materias" className="text-brand font-bold text-2xl w-fit my-1 hover:pl-1.5 hover:pr-5.5 hover:bg-purple hover:text-white ease-in transition-transform hover:translate-x-2"
+        >
+        Materias
+      </NavLink>
+
+      <NavLink 
+        to="/perfil" className="text-brand font-bold text-2xl w-fit my-1 hover:pl-1.5 hover:pr-5.5 hover:bg-secondary hover:text-white ease-in transition-transform hover:translate-x-2"
+        >
+        Perfil
+      </NavLink>
+      
+        </nav>
+    </div>
+  );
+}
+
+export default NavBarDsk;
