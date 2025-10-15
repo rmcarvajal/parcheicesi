a06810a999d8ad00c56aec9bd5aa21dc9e9c31fc
Juan Tovar <juanstovarm@gmail.com>
2025-10-15 16:04:45 -0500

    feat: Formulario de posts

--- src/components/Feed-components/Post-Interaction.tsx
+++ src/components/Feed-components/Post-Interaction.tsx
@@ -1,20 +1,50 @@
+import { useState } from "react";
 
+function PostInteractions () {
+    
+    
+    const heartOff = (
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-heart text-black" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+        </svg>
+    );
+    const heartOn = (
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-heart-fill text-brand" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+        </svg>
+    );
+    
+    //estado del boton y contador
+    const [liked, setLiked] = useState(false);
+    const [likes, setLikes] = useState(0);
+    
+    //cambia el estado de likes y el contador
+    const handleLike = () => {
+        setLiked(!liked);
+        setLikes(likes + (liked ? -1 : 1));
+    };
 
-function PostInteractions (){
-    const btnEffects = "hover:bg-brand-light hover:text-brand cursor-pointer active:bg-brand active:text-white w-full rounded-md p-1"
-    return(
+    //cambia el color del contador
+    const likedColors = liked? "text-brand": "text-black"
+    const btnState = liked? "bg-brand text-white font-bold": "bg-white text-black font-regular"
+
+    const btnEffects = "hover:bg-brand-light hover:text-brand cursor-pointer active:bg-brand active:text-white w-full rounded-md p-1 transition ";
+
+    return (
         <div className="p-2.5 flex flex-col">
             <div className="flex flex-row items-center h-fit gap-0.5">
-                <img className="h-5 w-5 mt-1" src="src\Assets\heart.svg"></img>
-                <h2 className="flex items-baseline text-lg">0</h2>
+                <button onClick={handleLike} className="focus:outline-none">
+                    {liked ? heartOn : heartOff}
+                </button>
+                <h2 id="like-counter" className={likedColors + " flex items-baseline text-lg"}>{likes}</h2>
             </div>
             <div className="flex justify-around">
-                <button className={btnEffects}>Like</button>
+                <button className={btnEffects + btnState} onClick={handleLike}>{liked ? "Liked" : "Like"}</button>
                 <button className={btnEffects}>Comment</button>
                 <button className={btnEffects}>Share</button>
             </div>
         </div>
-    )
+    );
 }
 
 export default PostInteractions
--- src/components/Feed-components/Post-List-Component.tsx
+++ src/components/Feed-components/Post-List-Component.tsx
@@ -1,19 +1,36 @@
 import { useState } from "react"
 import PostComponent from "./Post-Component"
 import data from "./FeedData.json"
-
+import PostForm from "./PostForm"
 function PostList (){
     const [posts] = useState(data)
-    
+    const [showForm, setShowForm] = useState(false)
 
     return(
         <div id="post-list-container" className="flex flex-col items-center gap-2.5 px-4 py-30 h-fit w-screen md:w-full md:py-5 md:inset-shadow-sm md:inset-shadow-black bg-gradient-to-b from-brand-light to-white">
             <div id="post-list-header" className="flex flex-row justify-between max-w-100 w-full">
-                <h1 className="font-bold text-2xl">Latest Posts</h1>
-                <button className="text-brand font-bold bg-white rounded-2xl p-2 px-6 border-1 hover:bg-brand hover:text-white hover:border-brand cursor-pointer">+ Añadir</button>
+                <h1 className="font-bold text-2xl">Ultimas publicaciones</h1>
+                <button
+                    id="añadir-btn"
+                    className="text-brand font-bold bg-white rounded-2xl p-2 px-6 border-1 hover:bg-brand hover:text-white hover:border-brand cursor-pointer"
+                    onClick={() => setShowForm(!showForm)}
+                >
+                    + Añadir
+                </button>
             </div>
+            {showForm && <PostForm />}
             <div id="post-list" className="flex flex-col items-center h-fit w-full max-w-lg gap-10.5">
-            {posts.map((p) => (<PostComponent key={p.id} user={p.user} pic={p.profilePic} time={p.time}text={p.text} img={p.image} cat={p.category}/> ))} 
+                {posts.map((p) => (
+                    <PostComponent
+                        key={p.id}
+                        user={p.user}
+                        pic={p.profilePic}
+                        time={p.time}
+                        text={p.text}
+                        img={p.image}
+                        cat={p.category}
+                    />
+                ))}
             </div>
         </div>
     )
