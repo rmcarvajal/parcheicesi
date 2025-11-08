0197b2840c6ed0e4f1d920edba295d52c6d88f9a
Daniel <135277854+Zaphkiel2200@users.noreply.github.com>
2025-11-08 17:33:25 -0500

    Hotfix

--- src/components/app/store.ts
+++ src/components/app/store.ts
@@ -1,13 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; 
 import postsReducer from '../features/postSlice'; 
-import authReducer from '../features/authSlice';   
+import authReducer from '../features/authSlice'; 
+
+const persistConfig = {
+  key: 'root', 
+  storage,
+  whitelist: ['auth'], 
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
 
 export const store = configureStore({
   reducer: {
+    auth: persistedAuthReducer, 
     posts: postsReducer,
-    auth: authReducer, 
   },
+  // Middleware para ignorar las acciones de persistencia
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,20 @@
-// main.tsx
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux';
-import { store } from './components/app/store.ts'; 
+import { Provider } from 'react-redux'
+import { store, persistor } from './components/app/store.ts' 
+import { PersistGate } from 'redux-persist/integration/react' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}> 
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}> 
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
