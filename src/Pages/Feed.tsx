f267af17bf52b7d1931f3c4c855cf3d554433e29
Juan Tovar <juanstovarm@gmail.com>
2025-11-25 02:37:38 -0500

    fix: try render 2

--- src/Pages/Feed.tsx
+++ src/Pages/Feed.tsx
@@ -1,8 +1,6 @@
 import PostList from "../components/Feed-components/Post-List-Component"
 import useMediaQuery from 'react-responsive';
 import Navbar from "../components/Navbar"
-import { RootState } from "../components/app/store";
-import { useSelector} from "react-redux";
 import HeaderFeed from "../components/navegation/HeaderFeed"
 
 
@@ -11,7 +9,6 @@ function Feed(){
   
 
     const dskSize = useMediaQuery({ minWidth: 768 });
-    const currentUser = useSelector((state: RootState) => state.auth.user);
     const navBarMvl = (
     <div className="fixed bottom-0 w-full z-5 bg-white">
       <Navbar />
