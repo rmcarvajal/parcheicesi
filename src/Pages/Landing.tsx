fdf0c646c690850031e93e3160154a3c54ff99ac
Juan Tovar <juanstovarm@gmail.com>
2025-11-25 13:06:19 -0500

    fix: styles

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import Logo from "../assets/LOGO grande fondo oscuro.png";
+import Logo from "../assets/LOGO grande fondo oscuro.png"
 
 function Landing() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-        extend: {
-            colors: {
-                'brand-primary': '#5353F1',
-            },
-            
-        }
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#5353F1",          // color principal (bg-brand, text-brand)
+        brandLight: "#C9C9F0",     // --color-brand-light
+        secondary: "#01BA77",      // --color-secondary
+        orange: "#FB5D28",         // --color-orange
+        purple: "#8D59F8",         // --color-purple
+        yellow: "#E2EB3D",         // --color-yellow
+      }
     }
-}
+  },
+  plugins: []
+};
